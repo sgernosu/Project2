11d8eed325a7254fb2ce671a6094ae949ed427bf
sgernosu <gernstetter.3@osu.edu>
2021-06-02 18:26:58 -0400

    Add basic HUD printing method

--- set_view.rb
+++ set_view.rb
@@ -1,14 +1,22 @@
+# frozen_string_literal: true
+
 # Receives an array of cards and prints them
 def print_board(cards)
   i = 0
-  for i in 0..cards.length()
-    print(cards[i])
-    if ((i + 1) % 3).zero?
-      puts
-    end
+  (0..cards.length).each do |i|
+    print cards[i]
+    puts if ((i + 1) % 3).zero?
   end
   puts
 end
 
-cards = [['one', 'oval', 'empty'], ['two', 'triangle', 'striped'], ['three', 'square', 'filled'], ['one', 'oval', 'empty'], ['two', 'triangle', 'striped'], ['three', 'square', 'filled'], ['one', 'oval', 'empty'], ['two', 'triangle', 'striped'], ['three', 'square', 'filled']]
+# Receives a hash of players and scores and prints them along with other HUD elements
+def print_hud(players)
+  players.each_pair { |key, value| puts "#{key}: #{value}" }
+  puts
+end
+
+cards = [%w[one oval empty], %w[two triangle striped], %w[three square filled], %w[one oval empty], 
+         %w[two triangle striped], %w[three square filled], %w[one oval empty], %w[two triangle striped],
+         %w[three square filled]]
 print_board(cards)
