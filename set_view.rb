c35f518dba7a7ebdd8d982afcd163707b24b5e27
sgernosu <gernstetter.3@osu.edu>
2021-06-02 19:32:48 -0400

    Started basic main menu method

--- set_view.rb
+++ set_view.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+# Receives a hash of players and scores and prints them along with other HUD elements
+def print_hud(players)
+  players.each_pair { |key, value| puts "#{key}: #{value}" }
+  puts
+end
+
 # Receives an array of cards and prints them
 def print_board(cards)
   i = 0
@@ -10,13 +16,9 @@ def print_board(cards)
   puts
 end
 
-# Receives a hash of players and scores and prints them along with other HUD elements
-def print_hud(players)
-  players.each_pair { |key, value| puts "#{key}: #{value}" }
-  puts
+# Prints the main menu and calls other methods depending on user input (not yet though)
+def main_menu()
+  puts '1) New Game'
+  puts '2) Settings'
+  puts '3) Exit'
 end
-
-cards = [%w[one oval empty], %w[two triangle striped], %w[three square filled], %w[one oval empty], 
-         %w[two triangle striped], %w[three square filled], %w[one oval empty], %w[two triangle striped],
-         %w[three square filled]]
-print_board(cards)
