d60601b04a7e242fedef051e0f61ae5ffc4e2fef
sgernosu <gernstetter.3@osu.edu>
2021-06-02 20:50:58 -0400

    Add settings menu, implement more main menu functionality

--- set_view.rb
+++ set_view.rb
@@ -16,19 +16,53 @@ def print_board(cards)
   puts
 end
 
+# Prints the settings menu, where the user can add, rename, and remove players
+def settings_menu(players)
+  puts "1) Add Player\n2) Rename Player\n3) Remove Player\n4) Back"
+  print "Select an option: "
+  option = gets.chomp
+  until option == '4'
+    case option
+    when "1"
+      print "Enter player name to add: "
+      name = gets.chomp
+      players = add_player(players, name)
+    when "2"
+      print "Enter player name to change: "
+      old_name = gets.chomp
+      print "Enter new name: "
+      new_name = gets.chomp
+      players = rename_player(players, old_name, new_name)
+    when '3'
+      print "Enter player name to remove: "
+      name = gets.chomp
+      players = remove_player(players, name)
+    else
+      puts "Please select an available option."
+    end
+    puts "1) Add Player\n2) Rename Player\n3) Remove Player\n4) Back"
+    print "Select an option: "
+    option = gets.chomp
+  end
+  players
+end
+
 # Prints the main menu and calls other methods depending on user input
 def main_menu(players)
-  puts '1) New Game\n2) Settings\n3) Exit'
-  print 'Select an option: '
-  case gets.chomp
-  when '1'
-    new_game(players) # This might be named something else
-  when '2'
-    settings_menu(players)
-  when '3'
-    exit
-  else
-    puts 'Please select an available option'
-    main_menu(players)
+  puts "1) New Game\n2) Settings\n3) Exit"
+  print "Select an option: "
+  option = gets.chomp
+  until option == '3'
+    case option
+    when "1"
+      new_game(players)
+    when "2"
+      players = settings_menu(players)
+    else
+      puts "Please select an available option."
+    end
+    puts "1) New Game\n2) Settings\n3) Exit"
+    print "Select an option: "
+    option = gets.chomp
   end
 end
